1a532e96be19f2d590a668509e581a6071d93e4d
Jouni Kantola <jouni@kantola.se>
2015-01-16 00:00:27 +0100

    Turnary turned

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    prod: (process.env.NODE_ENV && process.env.NODE_ENV.indexOf('prod') > -1) ? false : true
+    prod: (process.env.NODE_ENV && process.env.NODE_ENV.indexOf('prod') > -1) ? true : false 
 }
