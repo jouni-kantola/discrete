840449599a2d09616b784dd190048535344aa28c
Jouni Kantola <jouni@kantola.se>
2015-01-16 22:16:22 +0100

    First test stubs

---
+++ test/bootstrap-tests.js
@@ -0,0 +1,10 @@
+var discrete = require('../lib/discrete'),
+    expect = require('chai').expect
+
+describe('discrete', function() {
+
+    it('should be ok', function() {
+        return expect(discrete).to.be.ok
+    })
+
+})
